013ea722418c61e47f11f5d079b3ebcea388416f
nomadcoderkor <nomadcoder.kor@gmail.com>
2021-09-24 00:29:47 +0900

    IF Switch 문 정리

--- README.md
+++ README.md
@@ -114,6 +114,47 @@ func loopSum(numbers ...int) int {
 ```
 
 # IF ELSE
-  * 
+```go
+func isLogin(userName string) bool {
+	if len(userName) > 0 {
+		return true
+	}
+	return false
+}
+
+func handyScore(over int) string {
+  // 조건절에 변수설정을 할 수 있다.
+  // score 변수 생성후 세미콜론 다음 조건을 줄 수 있다.
+	if score := over + 72; score < 82 {
+		return "Single 골퍼"
+	}
+	return "휴.."
+}
+```
+---
+
+# Switch
+```go
+func handyScore(over int) string {
+	// 조건절에 변수설정을 할 수 있다.
+	// score 변수 생성후 세미콜론 다음 조건을 줄 수 있다.
+	// score := over + 72
+	switch score := over + 72; {
+	case score == 72:
+		return "이븐"
+	case score < 72:
+		return "와.. 언더.."
+	case score < 82:
+		return "싱글"
+	case score > 81 && score < 90:
+		return "80대 고수"
+	case score == 90:
+		return "보기플레이어"
+	case score > 90 && score < 100:
+		return "흠.. 애매하네.."
+	}
+	return "제발 연습좀 .."
+}
+```
 
 
--- main.go
+++ main.go
@@ -23,11 +23,39 @@ import (
 //   return result
 // }
 
-func isLogin(userName string) bool {
-	if len(userName) > 0 {
-		return true
+// func isLogin(userName string) bool {
+// 	if len(userName) > 0 {
+// 		return true
+// 	}
+// 	return false
+// }
+
+// func handyScore(over int) string {
+// 	if score := over + 72; score < 82 {
+// 		return "Single 골퍼"
+// 	}
+// 	return "휴.."
+// }
+
+func handyScore(over int) string {
+	// 조건절에 변수설정을 할 수 있다.
+	// score 변수 생성후 세미콜론 다음 조건을 줄 수 있다.
+	// score := over + 72
+	switch score := over + 72; {
+	case score == 72:
+		return "이븐"
+	case score < 72:
+		return "와.. 언더.."
+	case score < 82:
+		return "싱글"
+	case score > 81 && score < 90:
+		return "80대 고수"
+	case score == 90:
+		return "보기플레이어"
+	case score > 90 && score < 100:
+		return "흠.. 애매하네.."
 	}
-	return false
+	return "제발 연습좀 .."
 }
 
 func main() {
@@ -35,5 +63,6 @@ func main() {
 	// fmt.Println(len, upper)
 	// result := loopSum(2, 44, 5, 43, 8, 18)
 	// fmt.Println(result)
-	fmt.Println(isLogin(""))
+	// fmt.Println(isLogin("test"))
+	fmt.Println(handyScore(0))
 }
